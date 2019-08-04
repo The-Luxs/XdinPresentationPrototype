72bc5e33cb1e32878f94e9fc784e0233120d0903
Christopher Muturi <Muturi@kth.se>
2019-08-04 09:43:44 -0400

    Added a new list design to the home page, Also added a local banner.

---
+++ XdinPresentationPrototype/XdinPresentationPrototype/Models/HomeTextSection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XdinPresentationPrototype.Models
+{
+    class HomeTextSection
+    {
+        public string header { get; set; }
+        public string text { get; set; }
+        public ImageSource imageUrl { get; set; }
+
+    }
+}
